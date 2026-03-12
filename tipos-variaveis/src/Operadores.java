f1535774b5e1c781b5338f25f391e5fdfd227f82
Tavinho3107 <odmo2001@gmail.com>
2026-03-12 20:41:40 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 20;
+        String result = a == b ? "Verdadeiro" : "Falso";
+        System.out.println(result);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        final double PI = 3.1415;
+        System.out.println("O valor de PI é: " + PI);
+        String meuNome = "Pedro Sampaio";
+        System.out.println("Meu nome é: " + meuNome);
+     
     }
 }
