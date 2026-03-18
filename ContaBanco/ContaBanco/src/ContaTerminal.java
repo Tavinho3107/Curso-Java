1cb8f0e06ed7a7f59ac35c29c279682f38809d81
Tavinho3107 <odmo2001@gmail.com>
2026-03-18 20:53:00 -0300

    ENTREGA DO PROJETO FINAL

--- ContaBanco/ContaBanco/src/ContaTerminal.java
+++ ContaBanco/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO: Conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o nosso usuario
+
+        // Obter pela scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
+        try(Scanner scanner = new Scanner(System.in)){
+        scanner.useLocale(new Locale("US"));
+
+        System.out.println("Por favor, digite o numero da conta: ");
+        int Numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente 
+
+        System.out.println("Por favor, digite o numero da agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome completo do cliente: ");
+        String Nome = scanner.nextLine();
+
+        System.out.println("Por davor, digite o saldo da conta: ");
+        double Saldo = scanner.nextDouble();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.println("Olá " + Nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + Numero + " e seu saldo " + Saldo + " já está disponível para saque.");      
+        }
     }
 }
